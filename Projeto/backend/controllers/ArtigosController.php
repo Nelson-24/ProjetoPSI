b064a8f7b05970dfb877e5e1ab9b9ba6008be348
Nelson <2220858@my.ipleiria.pt>
2023-11-21 16:57:41 +0000

    primeiro commit do projeto

---
+++ Projeto/backend/controllers/ArtigosController.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace backend\controllers;
+
+class ArtigosController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
